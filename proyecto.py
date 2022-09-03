bad592fb7a612773acc47db46a63b7d6d50bac03
NetoAR10 <111334147+NetoAR10@users.noreply.github.com>
2022-09-02 23:44:12 -0500

    Add files via upload

--- proyecto.py
+++ proyecto.py
@@ -1,30 +1,55 @@
-print("Escribe la cantidad de dinero ahorrada: ")
-ahorro1=float(input())
+def cambio_dia(ingreso, ahorro_1, gasto):
+    ahorro_2 = ahorro_1 + ingreso - gasto
+    cambio = ahorro_2 - ahorro_1
+    return cambio
 
-print("Escribe la fecha de hoy en formato dd/mm/aaaa uno por uno")
-dia=input()
-mes=input()
-año=input()
-fecha=f"{dia}/{mes}/{año}"
+def budget(ingreso):
+    esenciales = ingreso * 0.6
+    guardar = ingreso * 0.3
+    lujo = ingreso * 0.1
+    print("esenciales: ", esenciales)
+    print("guardar en ahorros: ", guardar)
+    print("lujos y/o diversión: ", lujo)
 
-print(fecha)
+def armar_fecha(dia, mes, año):
+    fecha = f"{dia}/{mes}/{año}" #este es un fstring que incorpora variables
+    return fecha
+    
 
-print("Si hay ingresos, escribe aqui la cantidad: ")
-ingreso=float(input())
+print("MENU")
+print("1) budget")
+print("2) cambio del dia")
+print("3) introcuir fecha")
+print("4) historial (aun no sirve)")
 
-print("Si hay gastos, escribe aqui la cantidad: ")
-gasto=float(input())
+opcion=int(input())
+if(opcion == 1):
+    print("Introduce tu ingreso para armar tu budget")
+    ingreso = float(input())
+    budget(ingreso)
 
-ahorro2=ahorro1+ingreso-gasto
-cambio=ahorro2-ahorro1
-escenciales=ingreso*0.6
-guardar=ingreso*0.3
-lujo=ingreso*0.1
+elif(opcion == 2):
+    print("escribe la cantidad ahorrada: ")
+    ahorro_1 = float(input())
+    print("escribe tu ingreso: ")
+    ingreso = float(input())
+    print("escribe tus gastos: ")
+    gasto = float(input())
+    print("el cambio del dia fue: ")
+    print(cambio_dia(ingreso, ahorro_1, gasto))
 
-print(f"Tu dinero actual el dia de hoy es:  ${ahorro2:.2f}")
-print(f"El cambio que hubo del dia de ayer al dia de hoy es de:  ${cambio:.2f}")
-print("La distribucion recomendad de tus ingresos es la siguinete")
-print(f"Escenciales = ${escenciales:.2f}")
-print(f"Guardar en ahorros = ${guardar:.2f}")
-print(f"Lujos y/o diversion = ${lujo:.2f}")
+elif(opcion == 3):
+    print("Escribe la fecha de hoy en formato dd/mm/aaaa uno por uno")
+    print("dia: ")
+    dia = int(input())
+    print("mes: ")
+    mes = int(input())
+    print("año: ")
+    año = int(input()) 
+    print(armar_fecha(dia, mes, año))
 
+elif(opcion == 4):
+    print("el historial aun no existe")
+
+elif(opcion<1 or opcion>4):
+    print("elige opcion del 1 al 4")
